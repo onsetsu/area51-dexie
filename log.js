20f637d4be7a1d953b2e89e47eb51d53fe5e3563
onsetsu <stefan.ramson@hpi.de>
2020-03-26 10:15:22 +0100

    hard copy of https://github.com/onsetsu/understanding-zone.js-and-dexie-promises

---
+++ log.js
@@ -0,0 +1,25 @@
+function stack(omit=stack) {
+    const s = {};
+    Error.captureStackTrace(s, omit);
+    return s.stack.split('\n').slice(1).join('\n');
+}
+
+function log(x) {
+    let con = document.body.querySelector('#log');
+    if (!con) {
+        document.body.innerHTML += `<div id="log"></div>`;
+        return log(x);
+    }
+
+    con.innerHTML += `<div class="entry">${x}</div>`.replace(/\n/gm, '<br />').replace(/<root>/gm, '&lt;root>');
+}
+
+function logStack(title) {
+    let msg = title ? title + '\n' : '';
+    msg += stack(logStack);
+    log(msg);
+}
+
+function hr() {
+    log('<hr />');
+}
