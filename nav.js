05f24ffd0b4984741b32301775285e9a2b908508
onsetsu <stefan.ramson@hpi.de>
2020-03-26 14:41:05 +0100

    added area51 as dexie.Promise clone

--- nav.js
+++ nav.js
@@ -2,6 +2,7 @@ function insertNavigation() {
     const div = document.createElement('div');
     document.currentScript.replaceWith(div);
     div.innerHTML = `
+<a href="./area51.html">area51</a>
 <a href="./zone.js.html">zone.js</a>
 <a href="./dexie.html">Dexie Promises</a>
 `;
